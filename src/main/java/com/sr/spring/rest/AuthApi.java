6b0a81e572fc5beed1dd3af8b47d615aa17b2942
Falgor.C <tranhoa.7604@gmail.com>
2025-05-06 15:13:41 +0700

    add: log

--- src/main/java/com/sr/spring/rest/AuthApi.java
+++ src/main/java/com/sr/spring/rest/AuthApi.java
@@ -3,6 +3,9 @@ package com.sr.spring.rest;
 import com.sr.spring.dto.LoginRequest;
 import com.sr.spring.dto.LoginResponse;
 import com.sr.spring.service.AuthService;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -15,8 +18,14 @@ public class AuthApi {
     @Autowired
     private AuthService authService;
 
+    private Logger logger = LoggerFactory.getLogger(AuthApi.class);
+
     @PostMapping("/login")
     public LoginResponse login(@RequestBody LoginRequest loginRequest) {
+        logger.debug("Debugging log message");
+        logger.info("Information log message");
+        logger.warn("Warning log message");
+        logger.error("Error log message");
         String token = authService.authen(loginRequest.getUsername(), loginRequest.getPassword());
         return new LoginResponse(token);
     }
